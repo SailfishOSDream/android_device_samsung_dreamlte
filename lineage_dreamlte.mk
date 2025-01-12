04ff6a7b97056fc850a77560507d36814745ff85
foxclore <32997971+foxclore@users.noreply.github.com>
2025-01-12 22:41:41 +0200

    Update lineage_dreamlte.mk

--- lineage_dreamlte.mk
+++ lineage_dreamlte.mk
@@ -16,8 +16,8 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit from dreamlte device
 $(call inherit-product, device/samsung/dreamlte/device.mk)
